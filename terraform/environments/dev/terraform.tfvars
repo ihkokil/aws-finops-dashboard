713984ec95a59b9d309f2121d490832e52fb2f34
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2025-11-24 12:59:40 +0600

    feat(env): add dev environment

---
+++ terraform/environments/dev/terraform.tfvars
@@ -0,0 +1,7 @@
+account_id         = "123456789012"
+region             = "us-east-1"
+github_repo        = "ihkokil/aws-finops-dashboard"
+collector_image    = "123456789012.dkr.ecr.us-east-1.amazonaws.com/finops-collector:latest"
+subnet_ids         = ["subnet-0123456789abcdef0", "subnet-0fe2456789abcdef1"]
+security_group_ids = ["sg-0123456789abcdef0"]
+slack_webhook_url  = ""
---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,38 @@
+variable "account_id" {
+  type        = string
+  description = "AWS Account ID"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS Region"
+}
+
+variable "github_repo" {
+  type        = string
+  default     = "owner/aws-finops-dashboard"
+  description = "GitHub Repository string"
+}
+
+variable "collector_image" {
+  type        = string
+  default     = "123456789012.dkr.ecr.us-east-1.amazonaws.com/finops-collector:latest"
+  description = "Collector container image"
+}
+
+variable "subnet_ids" {
+  type    = list(string)
+  default = ["subnet-0123456789abcdef0"]
+}
+
+variable "security_group_ids" {
+  type    = list(string)
+  default = ["sg-0123456789abcdef0"]
+}
+
+variable "slack_webhook_url" {
+  type      = string
+  default   = ""
+  sensitive = true
+}
